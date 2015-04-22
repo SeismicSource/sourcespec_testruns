410fb53c1f471634d140b897aa490fddef26bf6e
Claudio Satriano <satriano@gmail.com>
2015-04-22 08:54:01 -0400

    Code style fixes.

--- testdata/CRL/test_dataless.py
+++ testdata/CRL/test_dataless.py
@@ -10,32 +10,32 @@ from datetime import timedelta
 from glob import glob
 
 for dlessfile in glob('dataless.*'):
-	try:
-		sp = Parser(dlessfile)
-	except IOError:
-		print 'Error reading file:', dlessfile
-		continue
+    try:
+        sp = Parser(dlessfile)
+    except IOError:
+        print 'Error reading file:', dlessfile
+        continue
 
-	blk = sp.blockettes
-	net = blk[50][0].network_code
-	sta = blk[50][0].station_call_letters
+    blk = sp.blockettes
+    net = blk[50][0].network_code
+    sta = blk[50][0].station_call_letters
 
-	startdates = [b.start_date for b in blk[52]]
-	#enddates = [b.end_date for b in blk[52]]
-	chans = [b.channel_identifier for b in blk[52]]
-	locs = [b.location_identifier for b in blk[52]]
+    startdates = [b.start_date for b in blk[52]]
+    #enddates = [b.end_date for b in blk[52]]
+    chans = [b.channel_identifier for b in blk[52]]
+    locs = [b.location_identifier for b in blk[52]]
 
-	for i in range(0, len(startdates)):
-		channel_id = net + '.' + sta + '.' + locs[i] + '.' + chans[i]
-		# If we do not add at least 7 minutes to the start_time, ObsPy says:
-		# "None or more than one channel with the given description"
-		# why????
-		t = timedelta(minutes=7)
-		time = startdates[i] #+ t
-		try:
-			PAZ = sp.getPAZ(channel_id, time)
-			#print PAZ
-			#coords = sp.getCoordinates(channel_id, time)
-			#print coords['longitude'], coords['latitude'], channel_id.split('.')[1]
-		except Exception, error:
-			print dlessfile, time, channel_id, error
+    for i in range(0, len(startdates)):
+        channel_id = net + '.' + sta + '.' + locs[i] + '.' + chans[i]
+        # If we do not add at least 7 minutes to the start_time, ObsPy says:
+        # "None or more than one channel with the given description"
+        # why????
+        t = timedelta(minutes=7)
+        time = startdates[i] #+ t
+        try:
+            PAZ = sp.getPAZ(channel_id, time)
+            #print PAZ
+            #coords = sp.getCoordinates(channel_id, time)
+            #print coords['longitude'], coords['latitude'], channel_id.split('.')[1]
+        except Exception, error:
+            print dlessfile, time, channel_id, error
