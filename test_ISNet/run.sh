4544bc646a263fb6dd4e87ce9dccc42c3357d8df
Claudio Satriano <satriano@gmail.com>
2020-04-23 17:55:04 +0200

    Fix typo

--- test_ISNet/run.sh
+++ test_ISNet/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Test run of source_spec on Irpinia Seismic Netowrk (ISNet, Italy) data
+# Test run of source_spec on Irpinia Seismic Network (ISNet, Italy) data
 source_spec -c config_ISNet.conf -t data/14641r.full.sac.tgz
